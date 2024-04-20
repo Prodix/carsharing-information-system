670d33e788643ecdb8fc222e2d9a8bb5ea2ab2dd
Prodix <prodixfg@gmail.com>
2024-04-20 19:22:14 +0300

    Добавлен метод загрузки повреждений

--- carsharing-api/carsharing-api/Controllers/TransportController.cs
+++ carsharing-api/carsharing-api/Controllers/TransportController.cs
@@ -1,4 +1,5 @@
 ﻿using carsharing_api.Context;
+using carsharing_api.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
@@ -66,5 +67,30 @@ public class TransportController : Controller
     {
         Response.Headers.ContentDisposition = "attachment";
         await Response.SendFileAsync(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "damages", name));
+    }
+    
+    [HttpPost]
+    [Route("/api/transport/damage/send")]
+    public IActionResult GetTransportTest()
+    {
+        var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "damages",
+            Request.Form.Files[0].FileName.Replace("jpeg", "jpg"));
+        
+        using (var file = new StreamWriter(path))
+        {
+            Request.Form.Files[0].CopyTo(file.BaseStream);
+            file.Flush();
+            
+            var damage = new Damage()
+            {
+                TransportId = Convert.ToInt32(Request.Form.Files[0].FileName[0].ToString()),
+                Path = Request.Form.Files[0].FileName.Replace("jpeg", "jpg")
+            };
+
+            _db.Damage.Add(damage);
+            _db.SaveChanges();
+        }
+
+        return StatusCode(200);
     }
 }
